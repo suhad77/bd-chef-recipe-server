3d4bf246e58e337a0794bf99a5d553c3ff88ce4b
Suhad Mia <suhadmia12@gmail.com>
2023-05-04 11:10:23 +0600

    json file send

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const categories = require('./data/categories.json');
+const categories = require('./data/categories.json');
 
-// app.get('/categories', (req, res)=>{
-//     res.send(categories);
-// })
+app.get('/categories', (req, res)=>{
+    res.send(categories);
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
