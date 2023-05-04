92306d877cc8702e6cfb1b77a0e53f620a452c19
Suhad Mia <suhadmia12@gmail.com>
2023-05-04 11:18:39 +0600

    cors file added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
+var cors = require('cors')
 const port = process.env.PORT || 5000;
+
+app.use(cors())
 
 const categories = require('./data/categories.json');
 
